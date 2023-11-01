8f8a2eb711e6cdbbbe3f2fc154356f32e030890e
Oto Mercado <otomerqui@gmail.com>
2023-11-01 16:41:11 -0500

    update on skills section

---
+++ cotizadorSeguros/app.js
@@ -0,0 +1,197 @@
+
+//Crear Objeto seguro
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+    //Llena las opciones de años
+    llenarOpciones();
+})
+
+//Función Realiza la cotización con los datos
+
+function calcularSeguro(seguro) {
+     /*
+    1. Americano 1.15
+    2.Asiatico 1.05
+    3. Europeo 1.35
+    */
+   
+   let cantidad;
+   const base = 2000;
+   
+   switch(this.marca.value) {
+    case '1':
+        cantidad = base*1.15;
+        break;
+    case '2':
+        cantidad = base*1.05;
+        break;
+    case '3':
+        cantidad = base*1.35;
+        break;
+   }
+
+   
+   //Leer el año
+   const diferencia = new Date().getFullYear() - this.year.value;
+  
+
+   //Cada año el costo va a reducirse un 3%
+   cantidad -= (( diferencia * 3) * cantidad) / 100;
+
+  
+   
+   /* 
+   Si el seguro es básico se mulstiplica por 30% más
+   Si el seguro es completo se mulstiplica por 50% más
+   */
+   
+   //console.log(seguro);
+ 
+   if(seguro.tipo === 'basico') {
+    cantidad *= 1.30;       
+    } else  {
+    cantidad *= 1.50; 
+    }
+
+return cantidad;
+   
+
+
+}
+
+//Función event listener
+eventListener();
+function eventListener() {
+    const formulario = document.querySelector('#formulario');
+
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+//Función cotizar seguro
+function cotizarSeguro(e) {
+    e.preventDefault();
+    
+
+    //Leer marca seleccionada
+    const marca = document.querySelector('#marca').value;    
+
+    //Leer año seleccionado
+    const year = document.querySelector('#year').value;    
+
+    //leer tipo de cobertura
+    const tipo = document.querySelector('input[type="radio"]:checked').value;
+    //console.log(tipo);
+
+    if(marca === '' || year === '' || tipo === '') {
+        mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
+    }
+
+    mostrarMensaje('Cotizando', 'exito');
+
+    //Ocultar cotizaciones previas
+    const resultados = document.querySelector('#resultado div');
+    if(resultados != null) {
+        resultados.remove();
+    }
+
+    //Instanciar el seguro
+    const seguro = new Seguro(marca, year, tipo);
+    //console.log(seguro);
+    
+    const total = calcularSeguro(seguro);
+    //console.log(seguro);
+    mostrarResultado(seguro,total);
+}
+
+function mostrarResultado(seguro,total) {
+    //console.log('desde mostrar resultado');
+
+    let textoMarca;
+    switch(seguro.marca) {
+        case '1':
+            textoMarca = 'Americano';
+            break;
+        case '2':
+            textoMarca = 'Asiatico';
+            break;
+        case '3':
+            textoMarca = 'Europeo';
+            break;   
+
+        default:
+            break;
+    }
+    //console.log(textoMarca);
+    //Mostrar el spinner
+    const spinner = document.querySelector('#cargando');
+    spinner.style.display = 'block';
+
+    setTimeout(() => {
+        spinner.style.display = 'none';//Se borra spinner y muestra el resultado
+       
+    }, 3000);
+    
+    
+    //Crear el resultado
+    setTimeout(() => {
+        const div = document.createElement('div');
+        div.classList.add('mt-10');
+        div.innerHTML = `
+            <p class="header"> Tu Resumen</p>
+            <p class="font-bold">Marca: <span class="font-normal"> ${textoMarca} </span></p>
+            <p class="font-bold">Año: <span class="font-normal"> ${seguro.year} </span></p>
+            <p class="font-bold">Tipo: <span class="font-normal capitalize"> ${seguro.tipo} </span></p>
+            <p class="font-bold">Total: <span class="font-normal"> $ ${total} </span></p>
+        `;
+        const resultadoDiv = document.querySelector('#resultado');
+        resultadoDiv.appendChild(div);
+    }, 3000);
+    
+
+    
+}
+
+//Función para mostrar mensajes de alerta en pantalla
+function mostrarMensaje(mensaje, tipo) {
+    const div = document.createElement('div');
+
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+    div.classList.add('mensaje');
+    div.textContent = mensaje;
+
+    //Insertar el HTML
+    const formulario = document.querySelector('#formulario');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    //Eliminar mensaje
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+}
+
+//Función llenar opciones de años
+function llenarOpciones() {
+    const max = new Date().getFullYear();
+    const min = max -20;
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i > min; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        selectYear.appendChild(opcion);
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,12 @@ const tabElements = document.querySelectorAll('button[role="tab"]');
 const panelElements = document.querySelectorAll('[role="tabpanel"]');
 let activeIndex = 0;
 
-console.log(tabElements);
+//console.log(tabElements);
 
 // Listen to clicks on tabs
 tabElements.forEach(function (tab, index) {
   tab.addEventListener("click", function (e) {
-    console.log(index);
+    //console.log(index);
     setActiveTab(index);
   });
 });
@@ -18,7 +18,7 @@ function setActiveTab(index) {
 
   // Set the new tab as active
   tabElements[index].setAttribute("aria-selected", "true");
-  console.log(tabElements[index]);
+  //console.log(tabElements[index]);
 
   setActivePanel(index);
   activeIndex = index;
@@ -33,3 +33,98 @@ function setActivePanel(index) {
   panelElements[index].style.display = 'flex';
   //console.log(panelElements[index]);
 }
+
+
+
+// Barras de progreso sección skills
+
+const myBars = document.querySelectorAll(".myBar");
+let animationExecuted = false;
+
+window.addEventListener("scroll", () => {
+  myBars.forEach(myBar => {
+    const rect = myBar.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    
+    if (rect.top <= windowHeight && rect.bottom >= 0 && !animationExecuted) {
+      animateBar(myBar);
+      
+    } 
+  });
+});
+
+function animateBar(myBar) {
+  
+  const targetWidth = myBar.getAttribute("data-width");
+  let currentWidth = 0;
+
+  if (myBar.style.width === "") {
+    myBar.style.width = "0%";
+  } else {
+    currentWidth = parseInt(myBar.style.width, 10);
+  }
+
+  if (currentWidth < targetWidth) {
+    let id = setInterval(frame, 10);
+
+    function frame() {
+      if (currentWidth >= targetWidth) {
+        clearInterval(id);
+      } else {
+        currentWidth++;
+        myBar.style.width = currentWidth + "%";
+        myBar.querySelector("span").innerText = currentWidth + "%";
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+/*
+const skillSection = document.querySelector('section#skills_menu');
+const myBars = document.querySelectorAll(".myBar");
+let animationExecuted = false;
+
+window.addEventListener("scroll", () => {
+  const rect = skillSection.getBoundingClientRect();
+  console.log(window.innerHeight);
+  console.log(rect.top);
+  
+  // Comprueba si la sección está en la vista y si la animación aún no se ha ejecutado
+  if (rect.top <= window.innerHeight && !animationExecuted) {
+    move();
+    animationExecuted = true; // Marcar que la animación se ha ejecutado
+  } else if (rect.top > window.innerHeight) {
+    // Restablecer la variable cuando el usuario sube por encima de la sección
+    animationExecuted = false;
+  }
+
+
+});
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.querySelectorAll(".myBar");
+    elem.forEach(elementos => {
+      var width = 1;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elementos.style.width = width + "%";
+        }
+      }    
+    });
+  }
+}
+*/
