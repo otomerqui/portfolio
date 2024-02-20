8105c6bed99f7deae168995d60bd335ac6c59ab7
Oto Mercado <otomerqui@gmail.com>
2024-02-20 11:44:33 -0500

    New design for project session

---
+++ resources/js/db.js
@@ -0,0 +1,114 @@
+const proyectos = [
+	{
+		titulo: 'Vet Patients Records',
+		descripcion: 'Application to keep track of patient admission to the veterinarian and the owners contact information.',
+		technology: ['CSS', 'HTML', 'JS', 'indexedDB'],
+		url: 'http://127.0.0.1:5500/vetPatients/index.html',		
+        imagen: './resources/img/vet-patients.webp'
+	},
+    {
+		titulo: 'Multistep Form',
+		descripcion: 'Multistep form to select different types of services in each step and with their respective validations.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/multistepForm/index.html',		
+        imagen: './resources/img/multistep-form.webp'
+	},
+	{
+		titulo: 'Cooking School',
+		descripcion: 'WordPress Theme Website for a cooking school with their respective post types to show the different existing cooking courses and their respective teachers..',
+		technology: ['CSS', 'HTML', 'JS', 'PHP', 'WordPress'],
+		url: 'https://demo2.digitalizatunegocio.club/',		
+        imagen: './resources/img/escuela-de-cocina.webp'
+	},
+	{
+		titulo: 'Susurros del Paraíso',
+		descripcion: 'WordPress Theme for Single page for local business that provides clinical coaching service.',
+		technology: ['CSS', 'HTML', 'JS', 'PHP', 'WordPress'],
+		url: 'https://demo.digitalizatunegocio.club/',		
+        imagen: './resources/img/susurros-del-paraiso.webp'
+	},
+	{
+		titulo: 'Weekly Expenses APP',
+		descripcion: 'Small application to keep track of weekly expenses and compare them with the budget.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/weeklyExpenses/index.html',		
+        imagen: './resources/img/weekly-expenses.webp'
+	},
+	{
+		titulo: 'Car Insurance Calculator',
+		descripcion: 'calculator of the amount of money to pay for car insurance depending on the type of car, the year and the type of insurance.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/cotizadorSeguros/cotizador.html',		
+        imagen: './resources/img/car-insurance-calculator-img.webp'
+	},
+	{
+		titulo: 'Gym Business',
+		descripcion: 'WordPress Theme Website for local gyms with their post types to show the different classes and teachers of each one.',
+		technology: ['CSS', 'HTML', 'JS', 'PHP', 'WordPress'],
+		url: 'https://demo.digitalizatunegocio.club/',		
+        imagen: './resources/img/susurros-del-paraiso.webp'
+	},
+	{
+		titulo: 'Car Listing Website',
+		descripcion: 'Car sales website with filter to search by car brand, year, minimum price, maximum price, number of doors, transmission type and color.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/carStoreListing/carListing.html',		
+        imagen: './resources/img/car-listing.webp'
+	},
+	{
+		titulo: 'Ecommerce Website',
+		descripcion: 'Ecommerce website home page with shopping cart made with local storage.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/ecommerceWebsite/ecommerceWebsite.html',		
+        imagen: './resources/img/ecommerce-website-home.webp'
+	},
+	{
+		titulo: 'Notes App',
+		descripcion: 'Small notes application where you can add notes, edit them, delete them',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/notesApp/index.html',		
+        imagen: './resources/img/notes-app.webp'
+	},
+	{
+		titulo: 'Basic Calculator',
+		descripcion: 'Basic calculator that allows you to add, subtract, multiply and divide.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/notesApp/index.html',		
+        imagen: './resources/img/calculator.webp'
+	},
+	{
+		titulo: 'Optin Page',
+		descripcion: 'Optin page to register for an online class to learn NLP.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/notesApp/index.html',		
+        imagen: './resources/img/optin-page.webp'
+	},
+	{
+		titulo: 'Contact Validation Form',
+		descripcion: 'Small contact form with validations in all its input fields.',
+		technology: ['CSS', 'HTML', 'JS'],
+		url: 'http://127.0.0.1:5500/notesApp/index.html',		
+        imagen: './resources/img/form validation.webp'
+	},
+	{
+		titulo: 'Corporate Website',
+		descripcion: 'Homepage of small corporate website with its service, contact, gallery, etc. sections.',
+		technology: ['CSS', 'HTML'],
+		url: 'http://127.0.0.1:5500/notesApp/index.html',		
+        imagen: './resources/img/corporate-website.webp'
+	},
+	{
+		titulo: 'CoffeShop Website',
+		descripcion: 'Homepage of small CoffeShop website with its service, contact, gallery, etc. sections.',
+		technology: ['CSS', 'HTML'],
+		url: 'http://127.0.0.1:5500/notesApp/index.html',		
+        imagen: './resources/img/coffeshop-website.webp'
+	},
+	{
+		titulo: 'Photo Camera',
+		descripcion: 'Homepage of small Photo camera website with its service, contact, gallery, etc. sections.',
+		technology: ['CSS', 'HTML'],
+		url: 'http://127.0.0.1:5500/notesApp/index.html',		
+        imagen: './resources/img/photography-website.webp'
+	},
+];
---
+++ resources/js/projects.js
@@ -0,0 +1,38 @@
+const proyectosContainer = document.querySelector('.column');
+console.log(proyectos);
+console.log(proyectosContainer);
+
+  proyectos.forEach(proyecto => {
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('project_card');
+
+    const projectCardImage = document.createElement('div');
+    projectCardImage.classList.add('project_card_image');
+    projectCardImage.innerHTML = `<div class="project_card_image_bg" style="background-image: url('${proyecto.imagen}');"></div>`;
+    projectCard.appendChild(projectCardImage);
+
+    const projectCardContent = document.createElement('div');
+    projectCardContent.classList.add('project_card_content');
+
+    const projectCardTitle = document.createElement('div');
+    projectCardTitle.classList.add('project_card_title');
+    projectCardTitle.innerHTML = `<h3>${proyecto.titulo}</h3><i class="fa-solid fa-arrow-up-right-from-square"></i>`;
+    projectCardContent.appendChild(projectCardTitle);
+    
+
+    const projectCardText = document.createElement('div');
+    projectCardText.classList.add('project_card_text');
+    projectCardText.innerHTML = `<p>${proyecto.descripcion}</p>`;
+    projectCardContent.appendChild(projectCardText);
+
+    const projectCardTech = document.createElement('div');
+    projectCardTech.classList.add('project_card_tech');
+
+    const techList = proyecto.technology.map(tech => `<li class="project_card_tech_item">${tech}</li>`).join('');
+    projectCardTech.innerHTML = `<ul class="project_card_tech_list">${techList}</ul>`;
+    projectCardContent.appendChild(projectCardTech);
+
+    projectCard.appendChild(projectCardContent);
+
+    proyectosContainer.appendChild(projectCard);
+  });
