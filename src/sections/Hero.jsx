3b7745741914764906d23992b30de77e5f66ad98
Otoniel <otomerqui@gmail.com>
2026-09-24 08:24:50 -0500

    Update images url

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,7 +25,7 @@ export const Hero = () => {
         {/* Background */ }
         <div className="absolute inset-0">
             <img 
-                src="/hero-bg.jpg" 
+                src={`${import.meta.env.BASE_URL}/hero-bg.jpg`}
                 alt="Hero Image" 
                 className="w-full h-full object-cover opacity-40"
             />
@@ -105,7 +105,7 @@ export const Hero = () => {
                         />
                         <div className="relative glass rounded-3xl p-2 glow-border">
                             <img 
-                                src="/oto-mercado.webp" 
+                                src={`${import.meta.env.BASE_URL}/oto-mercado.webp`}
                                 alt="Otoniel Mercado" 
                                 className="w-full aspect-4/5 object-cover rounded-2xl opacity-70"
                             />
